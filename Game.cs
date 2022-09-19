c29eead65e0321e31918d5e6cceea3febd158846
Cameliuu <calin.iacob@student.upt.ro>
2022-09-19 18:40:07 +0300

    Add board components

--- Game.cs
+++ Game.cs
@@ -2,5 +2,7 @@ namespace tic_tac_toe_bot;
 
 public class Game
 {
-    
-}
+    public static Player player1;
+    public static Player player2;
+    public char[,] board=new char[3, 3];
+    }
---
+++ Player.cs
@@ -0,0 +1,9 @@
+using Discord;
+
+namespace tic_tac_toe_bot;
+
+public class Player
+{
+    public static IUser player { get; set; }
+    public static char choice { get; set; }
+}
