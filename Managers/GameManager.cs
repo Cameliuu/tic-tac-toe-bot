7dd5d66b46180d9e09db7e7311d53caf31bfdbd0
Cameliuu <calin.iacob@student.upt.ro>
2022-09-19 21:07:10 +0300

    Add possibility to accept/refuse game offer

--- Managers/GameManager.cs
+++ Managers/GameManager.cs
@@ -12,32 +12,36 @@ public class GameManager
     public  static char[,] board=new char[3, 3]{{'1','2','3'},{'4','5','6'},{'7','8','9'}};
     public  static bool active { get; set; }
     public static char turn { get; set; }
+    
     public  static async Task StartGame(SocketUser user, ISocketMessageChannel channel,string player,SocketGuild guild)
     {
+        
         Player1 = new Player();
+        if (user.Username == player)
+        {
+            await channel.SendMessageAsync($"{player} nu te poti provoca singur!");
+            return;
+        }
+    
+        var builder = new ComponentBuilder()
+            .WithButton("Yes", "yes", style: ButtonStyle.Success)
+            .WithButton("No", "no", style: ButtonStyle.Danger);
+        await channel.SendMessageAsync($"{player}, {user.Mention} Te provoaca la un joc de X si 0.Accepti?", components:builder.Build());
         Player2 = new Player();
         var playerS=await CustomUserTypeReader.GetUserFromString(player, guild);
-       await channel.SendMessageAsync($"{playerS.Mention}");
-       await SetActive(user, playerS);
-       await BuildBoard(channel,"label");
-       
+        await SetActive(user, playerS);
+
     }
 
     public  static async Task SetActive(IUser player1, IUser player2)
     {
         
         active = true;
-        Console.WriteLine("1");
         turn = 'X';
-        Console.WriteLine("2");
         Player1.choice = 'X';
-        Console.WriteLine("3");
         Player2.name = player2.Username;
-        Console.WriteLine("4");
         Player2.choice = 'O';
-        Console.WriteLine("5");
         Player1.name = player1.Username;
-        Console.WriteLine("");
         Console.WriteLine($"{DateTime.Now}\tGame active");
     }
 
@@ -54,11 +58,6 @@ public class GameManager
             .WithButton(board[2,1].ToString(), "8", row: 1)
             .WithButton(board[2,2].ToString(), "9", row: 2);
         return builder;
-    }
-    public static async Task BuildBoard(ISocketMessageChannel channel,string text)
-    {
-       
-        await channel.SendMessageAsync("Here is a button!", components: GetBuilder().Result.Build());
     }
     
 }
